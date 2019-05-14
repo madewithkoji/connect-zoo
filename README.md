cc399b163f3ad7f43f94653c4d4422148fbee6e2
jones <jones@koji-projects.com>
2019-05-14 16:39:37 +0000

    connect zoo timeee

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# Koji P5 Scaffold
+# Connect Zoo
 
-*A great starting point for any p5 project*
+*A easy to play game thats mobile first and production ready.*
 
-## Getting Started
-Write your p5 code [here](#~/frontend/pages/HomePage/index.js).
+-------
+# Where to go
+1. Your Frontend code is located at /frontend/pages and the main game page is located in [the app directory](#~/frontend/app/index.js)
+2. 3 Visual Customization Controls (VCCs) have been added to the template. These can be quickly modified to your applicaiton
+    - [Images](#~/.koji/customization/images.json!visual) - Change the animals to something new and the background.
+    - [Strings](#~/.koji/customization/strings.json!visual) - Change the page content text & button text.
 
-## Support and suggestions
-Please feel free to [contact me](https://gokoji.com/profile/jones) if you have any suggestions!   
+## Any Questions?
 
+Join our [Discord Server](https://discord.gg/eQuMJF6) for Koji for any software questions or bugs.   
+Any ideas/issues/fixes/pull requests can get sent to either the [Github Repositotry](https://github.com/madewithkoji/connect-zoo)
+or by message to [the template creator](https://gokoji.com/profile/jones)
--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,26 +1,151 @@
-let img;
-let button;
+let balls = [];
+let gravity;
+let BALLSIZE = Math.floor(Math.sqrt(window.innerWidth*window.innerHeight / 50) / 2);
+let images;
+let backgroundImage;
+let selectedIndex = -1;
+let chain = [];
+let score = 0;
+let timing = 1000;
+let gameOver = false;
+let modalOpen = false;
+let overlay;
 
 function setup() {
-  // make a full screen canvas
-  createCanvas(window.innerWidth, window.innerHeight);
-  img = loadImage(Koji.config.images.mouse); // Load the image
+    const canvas = createCanvas(window.innerWidth, window.innerHeight);
+    canvas.parent('root');
+    
+    gravity = createVector(0, 0.1);
+    for(let i=0;i<50;i++) {
+        balls.push(new Ball(random(window.innerWidth), random(window.innerHeight), 0, Math.floor(random(6)), i));
+    }
+    images = [1,2,3,4,5,6].map((n) => loadImage(Koji.config.images[`ball${n}`]));
+    backgroundImage = loadImage(Koji.config.images.backgroundImage);
 
+    // setup a pwa install popover
+    if (!window.matchMedia('(display-mode: standalone)').matches) {
+      Koji.on('pwaPromptReady', () => {
+        window.pwaReady = true;
+        setTimeout(() => createModal(), 20000);
+      })
+    }
+}
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+  BALLSIZE = Math.floor(Math.sqrt(window.innerWidth*window.innerHeight / 50) / 2);
+}
+
+
+function mousePressed() {
+    if(gameOver) window.location.reload();
+}
+
+function mouseReleased() {    
+    // remove the balls
+    this.selectedIndex = -1;
+    if(chain.length >= 3) {
+        score += 100*(chain.length-2);
+        chain.forEach((c) => {
+            balls[c].position.y = random(100) + 100;
+            balls[c].position.x = random(window.innerWidth);
+            balls[c].type = Math.floor(random(6));
+        })
+    }
+    chain = [];
+}
+
+class Ball {
+    constructor(x, y, xvel, type, index) {
+        this.position = createVector(x, y);
+        this.velocity = createVector(xvel, 0);30
+        this.radius = BALLSIZE;
+        this.index = index;
+        this.type = type;
+    }
+
+    step() {
+        if(mouseIsPressed && collidePointCircle(mouseX, mouseY, this.position.x, this.position.y, BALLSIZE*2)) {
+            if(selectedIndex === -1 && chain.length === 0) {
+                selectedIndex = this.index;
+                chain.push(this.index);
+            } else if(selectedIndex != this.index && chain.reduce((c, i) => c + (i === this.index ? 1 : 0), 0) === 0) {
+                if(p5.Vector.sub(this.position, balls[selectedIndex].position).mag() < BALLSIZE*3 && this.type === balls[selectedIndex].type) {
+                    selectedIndex = this.index;
+                    chain.push(this.index);
+                }
+            }
+        }
+
+        if(window.innerHeight - (this.position.y + BALLSIZE) > 5) this.velocity.add(gravity);
+        this.velocity.setMag(this.velocity.mag()-0.01)
+        this.position.add(this.velocity);
+
+        balls.forEach((ball) => {
+            if(this.position.dist(ball.position) < BALLSIZE*2) {
+                let dist = p5.Vector.sub(ball.position, this.position);
+                this.position.sub(dist.setMag(1 + BALLSIZE*2 - dist.mag()));
+                if(this.index < ball.index) {
+                    let tmp = ball.velocity.mag();
+                    ball.velocity = dist.setMag(this.velocity.mag()*0.95);
+                    this.velocity = p5.Vector.mult(dist.setMag(tmp), -0.95);
+                }
+
+            }
+        });
+
+        if(this.position.x < BALLSIZE || this.position.x > window.innerWidth-BALLSIZE) this.velocity.x *= -0.95;
+        if(this.position.y < BALLSIZE || this.position.y > window.innerHeight-BALLSIZE) this.velocity.y *= -0.95;
+
+        if(this.position.x < BALLSIZE) this.position.x = BALLSIZE+1;
+        if(this.position.x > window.innerWidth-BALLSIZE) this.position.x = window.innerWidth-BALLSIZE-1;
+        if(this.position.y < BALLSIZE) this.position.y = BALLSIZE+1;
+        if(this.position.y > window.innerHeight-BALLSIZE) this.position.y = window.innerHeight-BALLSIZE-1;
+
+        image(images[this.type], this.position.x - BALLSIZE, this.position.y - BALLSIZE, BALLSIZE*2, BALLSIZE*2);
+    }
 }
 
 function draw() {
-  // set the background color from the configuration options
-  background(Koji.config.colors.backgroundColor);
+    background(backgroundImage);
+    if(modalOpen) {
+      if (window.matchMedia('(display-mode: standalone)').matches) {
+        modalOpen = false;
+        overlay.remove(); 
+      }
+      return;
+    }
+    balls.forEach((ball) => ball.step());
+    fill(0);
+    if(!mouseIsPressed) selectedIndex = -1;
+    if(selectedIndex !== -1){
+        circle(balls[selectedIndex].position.x, balls[selectedIndex].position.y, BALLSIZE/2);
+        strokeWeight(BALLSIZE/2);
+        stroke(0);
+        chain.map((c, i, ch) => i !== ch.length-1 && line(balls[c].position.x, balls[c].position.y, balls[ch[i+1]].position.x, balls[ch[i+1]].position.y));
+    }
 
-  // format our text
-  textSize(24);
-  fill(Koji.config.colors.textColor);
-  textAlign(CENTER);
+    strokeWeight(0);
+    textSize(32);
+    fill(Koji.config.colors.textColor);
+    text(score, 20, 40);
+}
 
-  // print out our text
-  text(Koji.config.strings.content, window.innerWidth / 2, 100);
+function collidePointCircle(x1, y1, x2, y2, dist) {
+    return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)) < dist/2;
+}
 
-  // setup an image to follow our mouse
-  let imageSize = 120;
-  image(img, mouseX - (imageSize / 2), mouseY - (imageSize / 2), imageSize, imageSize);
+function createModal() {
+    modalOpen = true;
+    overlay = createDiv().addClass('overlay');
+    const box = createDiv().addClass('box');
+    const boxtext = createDiv(Koji.config.strings.modalText).addClass('boxtext');
+    const button = createButton(Koji.config.strings.modalButtonText);
+    button.mousePressed(() => {
+        Koji.pwaPrompt();
+    });
+    box.child(boxtext)
+    box.child(button);
+    overlay.child(box);
+    overlay.parent('root');
 }
